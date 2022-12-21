2055d478527154097d27cc5b8572943515f9eec0
xulingqi0579111 <2867646659@qq.com>
2022-12-21 20:55:42 +0800

    123

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,1 @@
-package org.example;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("22");
-        System.out.println("33");
-    }
-}
+package org.example;public class Main {    public static void main(String[] args) {        System.out.println("Hello world!");        System.out.println("22");        System.out.println("33");    }}
