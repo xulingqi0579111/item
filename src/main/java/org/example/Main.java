d86ef51c9c8b88fbb4ba7a821d2b59bce709798f
xulingqi0579111 <2867646659@qq.com>
2022-12-21 20:51:26 +0800

    123

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("22");
-
+        System.out.println("33");
     }
 }
