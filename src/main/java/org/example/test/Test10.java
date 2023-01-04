ab654bb15402a28bcfb22b58f35fba6f97c8d0a3
xulingqi0579111 <2867646659@qq.com>
2023-01-04 20:29:09 +0800

    123

---
+++ src/main/java/org/example/test/Test10.java
@@ -0,0 +1,14 @@
+package org.example.test;
+
+public class Test10 {
+    public static void main(String[] args) {
+        //死循环
+/*        for(;;){
+            System.out.println("11");
+        }*/
+        while (true) {
+            System.out.println("22");
+        }
+    }
+
+}
