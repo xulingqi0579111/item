b244ddcd3a3cf1f5923e4ed8c8d9d30c5a4152e2
xulingqi0579111 <2867646659@qq.com>
2023-01-10 22:41:21 +0800

    123

--- src/main/java/org/example/test/Test23.java
+++ src/main/java/org/example/test/Test23.java
@@ -2,6 +2,17 @@ package org.example.test;
 
 public class Test23 {
     public static void main(String[] args) {
+        int[] arr = {33, 66, 22, 11, 55};
+        int[] temp=new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            temp[i]=arr[arr.length-1-i];
+        }
 
+        for (int i = 0; i < temp.length; i++) {
+            System.out.print(temp[i]);
+            if (i<temp.length-1){
+                System.out.print(",");
+            }
+        }
     }
 }
